2049f26eecf2fefc163daf3667da9dcee086c817
Antonio <aneto2024@jpkeefehs.org>
2024-02-09 10:52:17 -0500

    clean

--- db/testdump.sql
+++ db/testdump.sql
@@ -1,3 +1,4 @@
-CREATE TABLE IF NOT EXISTS users(id int AUTO_INCREMENT, displayName VARCHAR(100), email VARCHAR(320), pic VARCHAR(100), PRIMARY KEY (id));
+CREATE TABLE IF NOT EXISTS users(id int AUTO_INCREMENT, userid VARCHAR(100), displayName VARCHAR(100), email VARCHAR(320), password VARCHAR(96), PRIMARY KEY (id));
+CREATE TABLE IF NOT EXISTS aproval(id int AUTO_INCREMENT, userid VARCHAR(100), displayName VARCHAR(100), email VARCHAR(320), password VARCHAR(96), PRIMARY KEY (id));
 ALTER USER 'root' IDENTIFIED WITH mysql_native_password BY 'pass'; 
 flush privileges;
