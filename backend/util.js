e744d795d1033fa61fda6db4384c336c1aec6c1c
revilN0 <olliejwilkinson@gmail.com>
2025-10-07 08:31:02 -0400

    JSON stuff

--- backend/util.js
+++ backend/util.js
@@ -1,28 +1,6 @@
 const express = require('express');
 const router = express.Router();
 
-// If the client prefers JSON (Accept: application/json) or requests ?format=json or ?json=1,
-// override res.render to return the view locals as JSON. This makes existing routes
-// that call res.render(...) return JSON without changing each handler.
-router.use((req, res, next) => {
-  const wantsJson = (req.accepts && req.accepts(['html', 'json']) === 'json') || req.query.format === 'json' || req.query.json === '1';
-  if (wantsJson) {
-    const originalRender = res.render.bind(res);
-    res.render = function(view, options, callback) {
-      // normalize arguments: res.render(view, options, callback) or res.render(view, callback)
-      if (typeof options === 'function') {
-        callback = options;
-        options = {};
-      }
-      // If no options provided, send an empty object to keep response consistent
-      res.json(options || {});
-    };
-    // expose original render in case a route needs it
-    res._originalRender = originalRender;
-  }
-  next();
-});
-
 const mysql = require('mysql-await')
 const pool = mysql.createPool({
   host: "ktprog.com",
